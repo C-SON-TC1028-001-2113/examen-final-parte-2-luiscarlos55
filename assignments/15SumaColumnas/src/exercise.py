e9b9d212cbfbc9784c6ff7e333a694f3f7fe2930
luiscarlos55 <a01254044@itesm.mx>
2021-10-15 17:50:15 +0000

    EXAMEN PARTE 2

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,8 +1,27 @@
 
-
+def matriz():
+    lista = []
+    fila = int(input())
+    columna = int(input())
+    if fila > 0 and columna > 0: 
+        for i in range(fila):
+            lista.append([])
+            for j in range(columna):
+                n = int(input())
+                lista[i].append(n)
+    else: 
+        print('Error')
+    return lista
 def main():
-
-
+    sumatriz = matriz()
+    lista_columna = []
+    if len(sumatriz) > 0:
+        for i in range(len(sumatriz[0])):
+            count = 0
+            for j in range(len(sumatriz)):
+                count += sumatriz[j][i] 
+            lista_columna.append(count)
+        print(lista_columna)
 
 if __name__=='__main__':
     main()
